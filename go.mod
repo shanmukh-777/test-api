d226d3c2f60f4d61fae6ea811b6f8277a708daa1
shanmukh <shanmukh.tata@smartfalcon.io>
2023-09-26 16:37:01 +0530

    added api endpoint to generate sessionid

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module apitest
 
-go 1.21.1
+go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.9.1
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.45.11
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -16,6 +17,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -26,10 +28,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"bytes"
-	"image/png"
+	"encoding/base64"
 	"image/jpeg"
+	"image/png"
+	"log"
+	"net/http"
 	"os"
 	"time"
-    "encoding/base64"
-    "github.com/gin-gonic/gin"
-    "net/http"
-	"log"
-	"gopkg.in/natefinch/lumberjack.v2"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 type request struct {
 	Mimetype    string `json:"mime_type"`
@@ -19,17 +23,44 @@ type request struct {
 }
 
 
+type facescompare struct{
+	Liveface string `json:"live_face"`
+	Referface string `json:"ref_face"`
+	similaritythres float64 `json:"sim_thres"`
+}
 
+type awsConf struct{
+accessKey string
+secretKey string
+kmsKeyId string
+region string
+}
+type liveliness struct {
+	Uniquetoken string `json:"unique_token"`
+}
+
+var conf awsConf
+var awsConfig *aws.Config
 func main() {
 
 	SetLogsfilepath()
-	
-	// Create a new Gin router
-    router := gin.Default()
+
+	conf.accessKey=os.Getenv("AWS_ACCESS_KEY")
+    conf.secretKey=os.Getenv("AWS_SECRET_KEY")
+	conf.kmsKeyId=os.Getenv("KMS_KEY_ID")
+	conf.region=os.Getenv("AWS_REGION")
+   
+	awsConfig=aws.NewConfig()
+	creds:=credentials.NewStaticCredentials(conf.accessKey,conf.secretKey,"")
+	awsConfig.WithCredentials(creds)
+	awsConfig.WithRegion(conf.region)
 
     // Define a route that accepts a Base64 encoded image
+    router := gin.Default()
     router.POST("/insert-image", InsertBase64Image)
-
+	router.POST("/compare-face", CompareFaces)
+    router.POST("/create-liveliness-session", CreateLive)
+	// router.GET("/create-kmskey",CreateK)
     // Run the server on port 8080
     router.Run(":8080")
 }
@@ -129,3 +160,108 @@ func InsertBase64Image(c *gin.Context) {
 	log.Printf("Image saved successfully")
 
 }
+func CompareFaces(c *gin.Context){
+	var faces facescompare
+	
+    if err := c.ShouldBindJSON(&faces); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    mySession := session.Must(session.NewSession())
+	
+	svc := rekognition.New(mySession, awsConfig)
+
+	// similarityThreshold := &faces.similaritythres
+	// facesrequest.SetSimilarityThreshold(*similarityThreshold)
+	//source image
+	sourcebytes,err := base64.StdEncoding.DecodeString(faces.Liveface)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot decode b64"})
+		return
+	}
+	sourceImage := rekognition.Image{
+		Bytes: sourcebytes, // Replace with actual image data
+	}
+	// facesrequest.SetSourceImage(&sourceImage)
+	//reference image
+	refbytes,err := base64.StdEncoding.DecodeString(faces.Referface)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot decode b64"})
+		return
+	}
+	refImage := rekognition.Image{
+		Bytes: refbytes,
+	
+	}
+	// facesrequest.SetTargetImage(&refImage)
+    facesRequest := rekognition.CompareFacesInput {
+		SourceImage: &sourceImage,
+		TargetImage: &refImage,
+		SimilarityThreshold: &faces.similaritythres,	
+			}	
+
+
+
+	res,err:=svc.CompareFaces(&facesRequest)
+	if err !=nil{
+        c.JSON(http.StatusBadRequest, gin.H{"error": "error while sending api request to aws"})
+		return
+	}
+	faceDetails:= res.FaceMatches[0]
+	sim:=faceDetails.Similarity
+	conf:=faceDetails.Face.Confidence
+    c.JSON(http.StatusOK, gin.H{"similarity": sim,
+          "confidence": conf })
+	
+}	
+// func CreateK(c *gin.Context){
+// 	mylivSession := session.Must(session.NewSession())
+//     kmsge:=kms.New(mylivSession)
+	
+// 	input:=kms.CreateKeyInput{
+// 		Description: aws.String("my kms key") ,
+// 		KeyUsage: aws.String("ENCRYPT_DECRYPT"),
+
+// 	}
+// 	resut,err:=kmsge.CreateKey(&input)
+// 	if err !=nil{
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+// 		return	
+// 	}
+// 	keyi:=resut.KeyMetadata.KeyId
+// 	c.JSON(http.StatusOK,gin.H{"keyid": keyi})
+
+// }
+
+
+func CreateLive(c *gin.Context){
+
+	var livetoken liveliness
+	mylivSession := session.Must(session.NewSession())
+	
+	liv := rekognition.New(mylivSession,awsConfig)
+	
+	
+	limit:=int64(1)
+	outputconfi:=rekognition.CreateFaceLivenessSessionRequestSettings{
+		AuditImagesLimit: &limit ,
+	}
+	livenessfaces:=rekognition.CreateFaceLivenessSessionInput{
+		ClientRequestToken: &livetoken.Uniquetoken,
+		KmsKeyId:           &conf.kmsKeyId,
+		Settings:           &outputconfi,
+	}
+	responseB,err:=liv.CreateFaceLivenessSession( &livenessfaces)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"sessionID": responseB})
+
+}
+
+
+    
+
+	
+
